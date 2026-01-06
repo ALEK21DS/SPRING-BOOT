511022b24d2acca120c15654f60a5e99df7d4b2e
ALEK21DS <brandonalexpesantez@gmail.com>
2026-01-05 22:53:01 -0500

    feat: Add SaludoController and update application properties

---
+++ inventario/src/main/java/com/krakedev/inventario/SaludoController.java
@@ -0,0 +1,22 @@
+package com.krakedev.inventario;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/*
+    Controlador REST: Es una clase en Java que expone rutas HTTP (URLs) para que otros sistemas
+    puedan enviarle peticiones y recibir respuestas en formato JSON.
+*/
+
+@RestController //Indica que esta clase es un controlador REST
+@RequestMapping("/micontroller") // Define la ruta base para todas las rutas en este controlador
+public class SaludoController {
+
+    @GetMapping("/saludo") //Mapea las peticiones GET a micontroller/saludo
+    public String saludar() {
+        return "Hola desde Spring Boot!";
+    }
+}
