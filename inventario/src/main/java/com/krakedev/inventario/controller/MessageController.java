9cd579b812ee34f6b4d27ce98f50da7c250a7add
ALEK21DS <brandonalexpesantez@gmail.com>
2026-01-05 23:40:00 -0500

    feat: Enhance MessageController with message retrieval by ID

--- inventario/src/main/java/com/krakedev/inventario/controller/MessageController.java
+++ inventario/src/main/java/com/krakedev/inventario/controller/MessageController.java
@@ -2,12 +2,14 @@ package com.krakedev.inventario.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.krakedev.inventario.entity.Message;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/messages")
@@ -23,5 +25,15 @@ public class MessageController {
     @GetMapping
     public List<Message> listarMensajes(){
         return mensajes;
+    }
+
+
+    @GetMapping("/{id}")
+    public Message obtenerMensajePorId(@PathVariable int id){
+        Optional<Message> mensaje = mensajes.stream()
+        .filter(m -> m.getId() == id).
+        findFirst();
+
+        return mensaje.orElse(null);
     }
 }
