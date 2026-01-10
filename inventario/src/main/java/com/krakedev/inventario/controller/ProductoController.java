5bcc29561ba328f7da3aae40993fa2c502e67526
ALEK21DS <brandonalexpesantez@gmail.com>
2026-01-10 11:59:53 -0500

    feat: Add update and delete endpoints for Productos by id in ProductoController

--- inventario/src/main/java/com/krakedev/inventario/controller/ProductoController.java
+++ inventario/src/main/java/com/krakedev/inventario/controller/ProductoController.java
@@ -14,10 +14,11 @@ import com.krakedev.inventario.entity.Producto;
 import com.krakedev.inventario.service.ProductoService;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/productos")
@@ -26,29 +27,57 @@ public class ProductoController {
     private final ProductoService productoService;
 
     @PostMapping("/registrar")
-    public ResponseEntity<?> registrarProducto(@RequestBody Producto producto){
+    public ResponseEntity<?> registrarProducto(@RequestBody Producto producto) {
         Producto nuevoProducto = productoService.registraProducto(producto);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevoProducto);
     }
 
     @GetMapping
-    public ResponseEntity<List<Producto>> listarProducto(){
+    public ResponseEntity<List<Producto>> listarProducto() {
         List<Producto> productos = productoService.listarProducto();
         return ResponseEntity.ok(productos);
     }
 
     @GetMapping("/buscar/nombre/{nombre}")
-    public ResponseEntity<?> buscarPorNombre(@PathVariable String nombre){
+    public ResponseEntity<?> buscarPorNombre(@PathVariable String nombre) {
         Optional<Producto> producto = productoService.buscarPorNombre(nombre);
-        return producto.isPresent() ? ResponseEntity.ok(producto.get()) 
-        : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Producto no encontrado");
+        return producto.isPresent() ? ResponseEntity.ok(producto.get())
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Producto no encontrado");
     }
 
     @GetMapping("/buscar/id/{id}")
-    public ResponseEntity<?> buscarPorId(@PathVariable Long id){
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
         Optional<Producto> producto = productoService.buscarPorId(id);
-        return producto.isPresent() ? ResponseEntity.ok(producto.get()) 
-        : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Producto no encontrado");
+        return producto.isPresent() ? ResponseEntity.ok(producto.get())
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Producto no encontrado");
     }
-    
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<?> actualizarProducto(@PathVariable Long id, @RequestBody Producto producto) {
+        try {
+            Producto productoActualizado = new Producto();
+            productoActualizado.setNombreProducto(producto.getNombreProducto());
+            productoActualizado.setDescripcion(producto.getDescripcion());
+            productoActualizado.setCantidad(producto.getCantidad());
+            productoActualizado.setPrecio(producto.getPrecio());
+            productoActualizado.setEstadoProducto(producto.getEstadoProducto());
+
+            Producto productoBDD = productoService.actualizaProducto(id, productoActualizado);
+
+            return ResponseEntity.ok(productoBDD);
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+        }
+    }
+
+    @DeleteMapping("eliminar/{id}")
+    public ResponseEntity<?> eliminarProducto(@PathVariable Long id) {
+        try {
+            productoService.eliminarProducto(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+        }
+    }
+
 }
